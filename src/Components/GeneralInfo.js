cd18938e3c1f777481684519803d7c5e6a7498da
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2023-02-24 12:51:46 +0000

    install react app, add generalinfo component

---
+++ src/Components/GeneralInfo.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+export default class GeneralInfo extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         firstName: "",
+         lastName: "",
+         address: "",
+         email: "",
+         phone: "",
+         description: ""
+
+      }
+    }
+
+    changeInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        
+        console.log(this.state)
+    }
+
+  render() {
+    return (
+        <div>
+            GeneralInfo
+            <form>
+                <input name="firstName" type="text" onChange={this.changeInput}></input>
+                <input name="lastName" type="text" onChange={this.changeInput}></input> 
+                <input name="address" type="text" onChange={this.changeInput}></input> 
+                <input name="email" type="text" onChange={this.changeInput}></input> 
+                <input name="phone" type="text" onChange={this.changeInput}></input> 
+                <input name="description" type="text" onChange={this.changeInput}></input>            
+            </form>
+        
+        
+        </div>
+    )
+  }
+}
